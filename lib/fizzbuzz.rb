1f305bdcb7fc77cc38d3b39092636437c186c86c
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-19 12:49:06 +0100

    fizzbuzz returns fizzbuzz for multiples of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 class Fixnum
 
   def fizzbuzz
-    if self % 3 == 0
+    if self % 3 == 0 && self % 5 == 0
+      "fizzbuzz"
+    elsif self % 3 == 0
       "fizz"
     elsif self % 5 == 0
       "buzz"
